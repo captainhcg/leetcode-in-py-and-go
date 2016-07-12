f99cf8fa10463478aebafff6ac3bd315ec660ee3
Chongguang He <captainhcg@gmail.com>
2016-07-11 23:25:11 -0700

    Create 206-reverse-linked-list.go

---
+++ 206-reverse-linked-list.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre, cur, next *ListNode
+	cur = head
+	for cur != nil {
+		next = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
