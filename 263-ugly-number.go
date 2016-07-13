5c56f87d847c4f037bd501d74d74395e7c0eaf15
Chongguang He <captainhcg@gmail.com>
2016-07-12 22:38:16 -0700

    Create 263-ugly-number.go

---
+++ 263-ugly-number.go
@@ -0,0 +1,13 @@
+func isUgly(num int) bool {
+    if num <= 0 {
+        return false
+    } else {
+        return defactor(defactor(defactor(num, 5), 3), 2) == 1
+}
+
+func defactor(num int, prime int) int {
+    for num % prime == 0 {
+        num = num / prime
+    }
+    return num
+}
