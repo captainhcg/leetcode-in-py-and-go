5a44717d8008416239fba1609776699e1863807d
Chongguang He <captainhcg@gmail.com>
2016-08-01 20:08:46 -0700

    Create 213-house-robber-ii.go

---
+++ 213-house-robber-ii.go
@@ -0,0 +1,35 @@
+func rob(nums []int) int {
+    if len(nums) == 0{
+        return 0
+    }
+    if len(nums) == 1{
+        return nums[0]
+    }
+    v1 := rob_helper(nums[:len(nums)-1])
+    v2 := rob_helper(nums[1:len(nums)])
+    return max(v1, v2)
+}
+
+func rob_helper(nums []int) int {
+    if len(nums) == 1{
+        return nums[0]
+    }
+    max_v := make([]int, len(nums))
+    for idx := 0; idx < len(nums); idx += 1{
+        if idx == 0{
+            max_v[idx] = nums[0]
+        } else if idx == 1{
+            max_v[idx] = max(nums[0], nums[1])
+        } else {
+            max_v[idx] = max(max_v[idx-1], max_v[idx-2] + nums[idx])
+        }
+    }
+    return max(max_v[len(nums)-1], max_v[len(nums)-2])
+}
+
+func max(a, b int) int {
+    if a > b{
+        return a
+    }
+    return b
+}
