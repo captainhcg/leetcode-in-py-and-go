1468a7c458b6c72cc70e258740f92f4e956cf864
Chongguang He <captainhcg@gmail.com>
2016-08-05 16:34:33 -0700

    Create 155-min-stack.py

---
+++ 155-min-stack.py
@@ -0,0 +1,40 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if self.stack:
+            m = self.getMin()
+        else:
+            m = x
+        self.stack.append((x, min(x, m)))
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
