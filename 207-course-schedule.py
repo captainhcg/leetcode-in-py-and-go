7cd60666b980cee8d58d10bb263f4b0359ba639b
Chongguang He <captainhcg@gmail.com>
2016-08-09 23:43:59 -0700

    Create 207-course-schedule.py

---
+++ 207-course-schedule.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        inline = [0] * numCourses
+        outline = [[] for _ in xrange(numCourses)]
+        for o, i in prerequisites:
+            inline[i] += 1
+            outline[o].append(i)
+        
+        dq = collections.deque()
+        for idx, v in enumerate(inline):
+            if not v:
+                dq.append(idx)
+
+        
+        courses = 0
+        while dq:
+            course = dq.popleft()
+            courses += 1
+            for o in outline[course]:
+                inline[o] -= 1
+                if inline[o] == 0:
+                    dq.append(o)
+        
+        return courses == numCourses
