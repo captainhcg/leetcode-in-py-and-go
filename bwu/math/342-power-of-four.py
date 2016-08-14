acdecbb76176fc16da61c9031bf8a4a0e2f56b33
Bin Wu <nosrevi.wb@gmail.com>
2016-08-14 16:15:51 -0500

    Create 342-power-of-four.py

---
+++ bwu/math/342-power-of-four.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return num > 0 and num & (num - 1) == 0 and len("{0:b}".format(num)) % 2 == 1
