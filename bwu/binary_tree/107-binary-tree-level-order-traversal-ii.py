46e924da3434cf2eb82e7957c7942c833ab03c61
Bin Wu <nosrevi.wb@gmail.com>
2016-08-14 16:45:30 -0500

    Create 107-binary-tree-level-order-traversal-ii.py

---
+++ bwu/binary_tree/107-binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        ret = []
+        if root is None:
+            return ret
+            
+        queue = [root]
+        while queue:
+            level = []
+            n = len(queue)
+            for i in xrange(n):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+            ret.append(level)
+                    
+        return ret[::-1]
