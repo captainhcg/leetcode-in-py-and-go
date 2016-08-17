ee02ac94b268482754ffdd95cec0d38e6fc49222
Bin Wu <nosrevi.wb@gmail.com>
2016-08-16 19:41:23 -0500

    Create 160-intersection-of-two-linked-lists.py

---
+++ bwu/linked_list/160-intersection-of-two-linked-lists.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA and not headB:
+            return None
+            
+        pA, pB = headA, headB
+        while pA != pB:
+            pA = pA.next if pA else headB
+            pB = pB.next if pB else headA
+            
+        return pA
