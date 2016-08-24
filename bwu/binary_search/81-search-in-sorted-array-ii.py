8bd51d470d2462065afbbd8d1406c2ebf6d696bf
Bin Wu <nosrevi.wb@gmail.com>
2016-08-23 22:58:17 -0500

    Create 81-search-in-sorted-array-ii.py

---
+++ bwu/binary_search/81-search-in-sorted-array-ii.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(nums)
+        if not n:
+            return False
+            
+        l, r = 0, n - 1
+        while l + 1 < r:
+            mid = (l + r) / 2
+            if nums[mid] == target:
+                return True
+                
+            if nums[l] == nums[mid] or nums[l] == nums[r]:
+                l += 1
+                continue
+                
+            if nums[mid] > nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid
+                else:
+                    l = mid
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid
+                else:
+                    r = mid
+                    
+        return mid[l] == target or mid[r] == target
