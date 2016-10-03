294bca64885cfdbbb8016d15cfe271df91c9322d
Bin Wu <nosrevi.wb@gmail.com>
2016-10-03 08:24:49 -0500

    Create 229-majority-element-ii.py

---
+++ bwu/array/229-majority-element-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+            
+        count1, count2, cand1, cand2 = 0, 0, 0, 1
+        for n in nums:
+            if n == cand1:
+                count1 += 1
+            elif n == cand2:
+                count2 += 1
+            elif count1 == 0:
+                cand1, count1 = n, 1
+            elif count2 == 0:
+                cand2, count2 = n, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+                
+
+        return [i for i in (cand1, cand2) if nums.count(i) > len(nums) // 3]
