b2b61a93468b4cc3096d6b59d24cf9bd04f2bebf
Bin Wu <nosrevi.wb@gmail.com>
2016-10-15 21:38:46 -0700

    Create 91-decode-ways.py

---
+++ bwu/dp/91-decode-ways.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+            
+        dp = [1] + [0] * (len(s))
+        for i in xrange(1, len(s) + 1):
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+            if i > 1 and '09' < s[i - 2:i] <= '26':
+                dp[i] += dp[i - 2]
+                
+        return dp[len(s)]
