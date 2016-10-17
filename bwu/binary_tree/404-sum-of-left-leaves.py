472c86afb8ee47ad2653d72d155605a634340776
Bin Wu <nosrevi.wb@gmail.com>
2016-10-16 22:58:30 -0500

    Update 404-sum-of-left-leaves.py

--- bwu/binary_tree/404-sum-of-left-leaves.py
+++ bwu/binary_tree/404-sum-of-left-leaves.py
@@ -13,3 +13,26 @@ class Solution(object):
             
         sum += self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
         return sum
+
+    def sumOfLeftLeaves2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = 0
+        if not root:
+            return res
+            
+        stack = [root]
+        while stack:
+            node = stack.pop(0)
+            if node.left:
+                if not node.left.left and not node.left.right:
+                    res += node.left.val
+                else:
+                    stack.append(node.left)
+            if node.right:
+                if node.right.left or node.right.right:
+                    stack.append(node.right)
+                    
+        return res
