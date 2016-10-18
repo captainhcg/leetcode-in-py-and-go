f5bbfa45a3697c68f4398c53d99e402cc0ff012b
Bin Wu <nosrevi.wb@gmail.com>
2016-10-18 09:20:42 -0500

    Create 398-random-index-pick.py

---
+++ bwu/math/398-random-index-pick.py
@@ -0,0 +1,25 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        
+        :type nums: List[int]
+        :type numsSize: int
+        """
+        self.l = nums
+        
+
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        index, count, nums = -1, 0, self.l
+        for i in range(len(nums)):
+            if nums[i] == target:
+                if index == -1:
+                    index = i
+                elif random.randint(0,count) == 0:
+                    index = i
+                count += 1
+        return index
