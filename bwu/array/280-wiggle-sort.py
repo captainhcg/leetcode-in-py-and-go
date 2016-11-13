ef1c4ee9c880c5bdf8b94fbd0f685f24b6f0534f
Bin Wu <nosrevi.wb@gmail.com>
2016-11-12 19:49:12 -0600

    Create 280-wiggle-sort.py

---
+++ bwu/array/280-wiggle-sort.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in xrange(1, len(nums)):
+            if i % 2 == 1 and nums[i - 1] > nums[i] or i % 2 == 0 and nums[i - 1] < nums[i]:
+                nums[i - 1], nums[i] = nums[i], nums[i - 1]
+        return
